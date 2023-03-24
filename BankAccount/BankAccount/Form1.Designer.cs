04b971d1346286c3a80ca1c95fa96c0f4f8a5853
IAMDOGG <124060531+IAMDOGG@users.noreply.github.com>
2023-03-24 21:47:43 +0800

    Add files via upload

---
+++ BankAccount/BankAccount/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace BankAccount
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rd_Deposit = new System.Windows.Forms.RadioButton();
+            this.rd_Check = new System.Windows.Forms.RadioButton();
+            this.rd_ServiceChange = new System.Windows.Forms.RadioButton();
+            this.btn_Calculate = new System.Windows.Forms.Button();
+            this.btn_Clear = new System.Windows.Forms.Button();
+            this.btn_Exit = new System.Windows.Forms.Button();
+            this.btn_Summary = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(46, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Amount ";
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(159, 110);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(133, 20);
+            this.txtAmount.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rd_ServiceChange);
+            this.groupBox1.Controls.Add(this.rd_Check);
+            this.groupBox1.Controls.Add(this.rd_Deposit);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(52, 189);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(240, 179);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Transaction";
+            // 
+            // rd_Deposit
+            // 
+            this.rd_Deposit.AutoSize = true;
+            this.rd_Deposit.Location = new System.Drawing.Point(23, 50);
+            this.rd_Deposit.Name = "rd_Deposit";
+            this.rd_Deposit.Size = new System.Drawing.Size(82, 24);
+            this.rd_Deposit.TabIndex = 0;
+            this.rd_Deposit.TabStop = true;
+            this.rd_Deposit.Text = "Deposit";
+            this.rd_Deposit.UseVisualStyleBackColor = true;
+            this.rd_Deposit.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // rd_Check
+            // 
+            this.rd_Check.AutoSize = true;
+            this.rd_Check.Location = new System.Drawing.Point(23, 92);
+            this.rd_Check.Name = "rd_Check";
+            this.rd_Check.Size = new System.Drawing.Size(72, 24);
+            this.rd_Check.TabIndex = 0;
+            this.rd_Check.TabStop = true;
+            this.rd_Check.Text = "Check";
+            this.rd_Check.UseVisualStyleBackColor = true;
+            // 
+            // rd_ServiceChange
+            // 
+            this.rd_ServiceChange.AutoSize = true;
+            this.rd_ServiceChange.Location = new System.Drawing.Point(23, 134);
+            this.rd_ServiceChange.Name = "rd_ServiceChange";
+            this.rd_ServiceChange.Size = new System.Drawing.Size(139, 24);
+            this.rd_ServiceChange.TabIndex = 0;
+            this.rd_ServiceChange.TabStop = true;
+            this.rd_ServiceChange.Text = "Service Change";
+            this.rd_ServiceChange.UseVisualStyleBackColor = true;
+            // 
+            // btn_Calculate
+            // 
+            this.btn_Calculate.Location = new System.Drawing.Point(518, 153);
+            this.btn_Calculate.Name = "btn_Calculate";
+            this.btn_Calculate.Size = new System.Drawing.Size(145, 33);
+            this.btn_Calculate.TabIndex = 4;
+            this.btn_Calculate.Text = "Calculate";
+            this.btn_Calculate.UseVisualStyleBackColor = true;
+            this.btn_Calculate.Click += new System.EventHandler(this.btn_Calculate_Click);
+            // 
+            // btn_Clear
+            // 
+            this.btn_Clear.Location = new System.Drawing.Point(518, 207);
+            this.btn_Clear.Name = "btn_Clear";
+            this.btn_Clear.Size = new System.Drawing.Size(145, 39);
+            this.btn_Clear.TabIndex = 4;
+            this.btn_Clear.Text = "Clear";
+            this.btn_Clear.UseVisualStyleBackColor = true;
+            this.btn_Clear.Click += new System.EventHandler(this.btn_Clear_Click);
+            // 
+            // btn_Exit
+            // 
+            this.btn_Exit.Location = new System.Drawing.Point(518, 267);
+            this.btn_Exit.Name = "btn_Exit";
+            this.btn_Exit.Size = new System.Drawing.Size(145, 38);
+            this.btn_Exit.TabIndex = 4;
+            this.btn_Exit.Text = "Exit";
+            this.btn_Exit.UseVisualStyleBackColor = true;
+            this.btn_Exit.Click += new System.EventHandler(this.btn_Exit_Click);
+            // 
+            // btn_Summary
+            // 
+            this.btn_Summary.Location = new System.Drawing.Point(518, 323);
+            this.btn_Summary.Name = "btn_Summary";
+            this.btn_Summary.Size = new System.Drawing.Size(145, 35);
+            this.btn_Summary.TabIndex = 4;
+            this.btn_Summary.Text = "Summary";
+            this.btn_Summary.UseVisualStyleBackColor = true;
+            this.btn_Summary.Click += new System.EventHandler(this.btn_Summary_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_Summary);
+            this.Controls.Add(this.btn_Exit);
+            this.Controls.Add(this.btn_Clear);
+            this.Controls.Add(this.btn_Calculate);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rd_ServiceChange;
+        private System.Windows.Forms.RadioButton rd_Check;
+        private System.Windows.Forms.RadioButton rd_Deposit;
+        private System.Windows.Forms.Button btn_Calculate;
+        private System.Windows.Forms.Button btn_Clear;
+        private System.Windows.Forms.Button btn_Exit;
+        private System.Windows.Forms.Button btn_Summary;
+    }
+}
+
---
+++ BankAccount/BankAccount/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace BankAccount
+{
+    public partial class Form1 : Form
+    {
+        private decimal balance = 0;
+        private int depositCount = 0;
+        private decimal depositTotal = 0;
+        private int checkCount = 0;
+        private decimal checkTotal = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Calculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                decimal amount = decimal.Parse(txtAmount.Text);
+                if (rd_Deposit.Checked)
+                {
+                    balance += amount;
+                    depositCount++;
+                    depositTotal += amount;
+                }
+                else if (rd_Check.Checked)
+                {
+                    if (balance >= amount)
+                    {
+                        balance -= amount;
+                        checkCount++;
+                        checkTotal += amount;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Insufficient Funds");
+                        balance -= 10;
+                        rd_ServiceChange.Checked = true;
+                        rd_ServiceChange.Enabled = false;
+                    }
+                }
+                else if (rd_ServiceChange.Checked)
+                {
+                    balance -= amount;
+                }
+                if (balance < 0)
+                {
+                    MessageBox.Show("New Balance is Negative");
+                }
+
+
+                MessageBox.Show("Balance: ₱" + balance.ToString("#,##0.00"));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_Clear_Click(object sender, EventArgs e)
+        {
+            rd_Deposit.Checked = true;
+            rd_ServiceChange.Enabled = true;
+            txtAmount.Clear();
+            txtAmount.Focus();
+        }
+
+        private void btn_Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_Summary_Click(object sender, EventArgs e)
+        {
+            string message =
+           "Deposit Count: " + depositCount + "\n" +
+           "Deposit Total: ₱" + depositTotal.ToString("#,##0.00") + "\n" +
+           "Check Count: " + checkCount + "\n" +
+           "Check Total: ₱" + checkTotal.ToString("#,##0.00");
+            MessageBox.Show(message);
+        }
+    }
+}
